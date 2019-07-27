77fa4478fae0e8f58033a8345d025afb8f6bb442
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-07-27 01:13:18 -0300

    Modulo4 - Task2 - Punto5: Funcionando

--- salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -3,7 +3,9 @@ package com.codeoftheweb.salvo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import static java.util.stream.Collectors.toList;
 
 @RestController
@@ -14,7 +16,14 @@ public class SalvoController {
     private GameRepository gRepository;
 
     @RequestMapping("/games")
-    public List<Object> getGameIDs(){
-        return gRepository.findAll().stream().map(g -> g.getId()).collect(toList());
+    public List<Object> getGameInfo(){
+        return gRepository.findAll().stream().map(g -> makeGameDTO(g)).collect(toList());
+    }
+
+    private Map<String, Object> makeGameDTO(Game game){
+        Map<String, Object> dto = new LinkedHashMap<String, Object>();
+        dto.put("id",game.getId());
+        dto.put("created", game.getCreationDate());
+        return dto;
     }
 }
